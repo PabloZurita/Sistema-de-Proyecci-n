783807ee8fe890c69f4a68444cc73302abe7ad4a
carloscr <carlos.caceresr@usach.cl>
2016-12-02 03:05:42 -0300

    Gráfico ISN histórico consultando a la base de datos

--- app/controllers/historico_controller.rb
+++ app/controllers/historico_controller.rb
@@ -1,6 +1,16 @@
 class HistoricoController < ApplicationController
 
   def index
-  	@indicadoresdiarios = Indicadoresdiario.all
+  	@indicadoresdiarios = Indicadoresdiario.order(fecha: :asc).all
+  	@datos
+  	if @indicadoresdiarios.length > 0 then
+  		@datos = Array.new(@indicadoresdiarios.length) { Array.new(2,0.0) }
+	 	for i in 0..@indicadoresdiarios.length-1
+	 		@datos[i][0] = (Time.zone.parse(@indicadoresdiarios[i].fecha.to_s).utc.to_f*1000).to_f
+	 		@datos[i][1] = @indicadoresdiarios[i].isn.to_f
+	  	end
+	else
+		@datos = [0,0] 
+	end
   end
 end
