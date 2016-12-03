4887ca96b2c78d2fc743caadc783cfe9fe720d84
PabloZurita <Pablo.zuritas@gmail.com>
2016-12-03 17:04:27 -0300

    resolutividad, tabla historico

--- app/controllers/vista2_controller.rb
+++ app/controllers/vista2_controller.rb
@@ -10,10 +10,12 @@ class Vista2Controller < ApplicationController
       @isn_diario = 0
       @resp_1_2_diario = 0
       @resp_4_5_diario = 0
+      @resolu_diario = 0
     else
       @isn_diario = @indicadoresdiarios.where(fecha: Time.now).where(segmento: id_segmento).take.isn
       @resp_1_2_diario = @indicadoresdiarios.where(fecha: Time.now).where(segmento: id_segmento).take.resp_1_2
       @resp_4_5_diario = @indicadoresdiarios.where(fecha: Time.now).where(segmento: id_segmento).take.resp_4_5
+      @resolu_diario = @indicadoresdiarios.where(fecha: Time.now).where(segmento: id_segmento).take.resolutividad
     end
     #@indicadoresdiarios = Indicadoresdiario.find(Indicadoresdiario.where(fecha: Time.parse(Time.now.to_s).strftime("%Y-%m-01")..Time.now).where(segmento: id_segmento).ids).isn
 
@@ -22,10 +24,12 @@ class Vista2Controller < ApplicationController
       @isn_acumulado = 0
       @resp_1_2_acumulado = 0
       @resp_4_5_acumulado = 0
+      @resolu_acumulado = 0
     else
       @isn_acumulado = @indicadoresacumulados.where(fecha: Time.now).where(segmento: id_segmento).take.isn
       @resp_1_2_acumulado = @indicadoresacumulados.where(fecha: Time.now).where(segmento: id_segmento).take.resp_1_2
       @resp_4_5_acumulado = @indicadoresacumulados.where(fecha: Time.now).where(segmento: id_segmento).take.resp_4_5
+      @resolu_acumulad = @indicadoresacumulados.where(fecha: Time.now).where(segmento: id_segmento).take.resolutividad
     end
     #@indicadoresacumulados = Indicadoresacumulado.find(Indicadoresacumulado.where(fecha: Time.parse(Time.now.to_s).strftime("%Y-%m-01")..Time.now).where(segmento: id_segmento).ids)
     #se supone que esta y la de más arriba da los ids del mes actual, pero no sé como acceder al isn
