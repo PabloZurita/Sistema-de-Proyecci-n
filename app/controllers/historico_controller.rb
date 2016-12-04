7c45c3e6fef3ce8ea73d867ff795d60f70183dc2
carloscr <carlos.caceresr@usach.cl>
2016-12-04 19:02:28 -0300

    Vista histórico - comparación de segmentos y global

--- app/controllers/historico_controller.rb
+++ app/controllers/historico_controller.rb
@@ -1,16 +1,80 @@
 class HistoricoController < ApplicationController
 
   def index
-  	@indicadoresdiarios = Indicadoresdiario.order(fecha: :asc).all
-  	@datos
-  	if @indicadoresdiarios.length > 0 then
-  		@datos = Array.new(@indicadoresdiarios.length) { Array.new(2,0.0) }
-	 	for i in 0..@indicadoresdiarios.length-1
-	 		@datos[i][0] = (Time.zone.parse(@indicadoresdiarios[i].fecha.to_s).utc.to_f*1000).to_f
-	 		@datos[i][1] = @indicadoresdiarios[i].isn.to_f
+  	@nombres_segmentos = ['Contrato','Hibrido','Prepago','Fijo','Pyme Fijo','Pyme Movil','Global Desktop'];
+  	@datos_desktop_global_diario
+  	@desktop_global_diario = Indicadoresdiario.where(segmento: 7).order(fecha: :asc)
+
+
+  	@indicadoresdiarios = Indicadoresdiario.where(segmento: 7).order(fecha: :asc)
+
+  	
+  	@datos_desktop_contrato_diario
+  	@desktop_contrato_diario = Indicadoresdiario.where(segmento: 1).order(fecha: :asc)
+  	
+  	@datos_desktop_hibrido_diario
+  	@desktop_hibrido_diario= Indicadoresdiario.where(segmento: 2).order(fecha: :asc)
+  	
+  	@datos_desktop_prepago_diario
+  	@desktop_prepago_diario = Indicadoresdiario.where(segmento: 3).order(fecha: :asc)
+  	
+  	@datos_desktop_fijo_diario
+  	@desktop_fijo_diario = Indicadoresdiario.where(segmento: 4).order(fecha: :asc)
+
+  	@datos_desktop_pyme_fijo_diario
+	@desktop_pyme_fijo_diario = Indicadoresdiario.where(segmento: 5).order(fecha: :asc)	
+
+  	@datos_desktop_pyme_movil_diario
+  	@desktop_pyme_movil_diario = Indicadoresdiario.where(segmento: 6).order(fecha: :asc)
+
+  	if @desktop_global_diario.length > 0 then
+  		@datos_desktop_global_diario = Array.new(@desktop_global_diario.length) { Array.new(2,0.0) }
+  		@datos_desktop_contrato_diario = Array.new(@desktop_contrato_diario.length) { Array.new(2,0.0) }
+  		@datos_desktop_hibrido_diario = Array.new(@desktop_hibrido_diario.length) { Array.new(2,0.0) }
+  		@datos_desktop_prepago_diario = Array.new(@desktop_prepago_diario.length) { Array.new(2,0.0) }
+  		@datos_desktop_fijo_diario = Array.new(@desktop_fijo_diario.length) { Array.new(2,0.0) }
+  		@datos_desktop_pyme_fijo_diario = Array.new(@desktop_pyme_fijo_diario.length) { Array.new(2,0.0) }
+  		@datos_desktop_pyme_movil_diario = Array.new(@desktop_pyme_movil_diario.length) { Array.new(2,0.0) }
+	 	for i in 0..@desktop_global_diario.length-1
+	 		@datos_desktop_global_diario[i][0] = (Time.zone.parse(@desktop_global_diario[i].fecha.to_s).utc.to_f*1000).to_f
+	 		@datos_desktop_global_diario[i][1] = @desktop_global_diario[i].isn.to_f
+
+	 		@datos_desktop_contrato_diario[i][0] = (Time.zone.parse(@desktop_contrato_diario[i].fecha.to_s).utc.to_f*1000).to_f
+	 		@datos_desktop_contrato_diario[i][1] = @desktop_contrato_diario[i].isn.to_f
+
+	 		@datos_desktop_hibrido_diario[i][0] = (Time.zone.parse(@desktop_hibrido_diario[i].fecha.to_s).utc.to_f*1000).to_f
+	 		@datos_desktop_hibrido_diario[i][1] = @desktop_hibrido_diario[i].isn.to_f
+
+	 		@datos_desktop_prepago_diario[i][0] = (Time.zone.parse(@desktop_prepago_diario[i].fecha.to_s).utc.to_f*1000).to_f
+	 		@datos_desktop_prepago_diario[i][1] = @desktop_prepago_diario[i].isn.to_f
+
+	 		@datos_desktop_fijo_diario[i][0] = (Time.zone.parse(@desktop_fijo_diario[i].fecha.to_s).utc.to_f*1000).to_f
+	 		@datos_desktop_fijo_diario[i][1] = @desktop_fijo_diario[i].isn.to_f
+
+	 		@datos_desktop_pyme_fijo_diario[i][0] = (Time.zone.parse(@desktop_pyme_fijo_diario[i].fecha.to_s).utc.to_f*1000).to_f
+	 		@datos_desktop_pyme_fijo_diario[i][1] = @desktop_pyme_fijo_diario[i].isn.to_f
+
+	 		@datos_desktop_pyme_movil_diario[i][0] = (Time.zone.parse(@desktop_pyme_movil_diario[i].fecha.to_s).utc.to_f*1000).to_f
+	 		@datos_desktop_pyme_movil_diario[i][1] = @desktop_pyme_movil_diario[i].isn.to_f
+
+
 	  	end
+	  	@datos_diario = [@datos_desktop_global_diario,
+	  		@datos_desktop_contrato_diario,
+	  		@datos_desktop_hibrido_diario,
+	  		@datos_desktop_prepago_diario,
+	  		@datos_desktop_fijo_diario,
+	  		@datos_desktop_pyme_fijo_diario,
+	  		@datos_desktop_pyme_movil_diario
+	  	]
 	else
-		@datos = [0,0] 
+		@datos_desktop_global_diario = [0,0]
+		@datos_desktop_contrato_diario = [0,0]
+		@datos_desktop_hibrido_diario = [0,0]
+		@datos_desktop_prepago_diario = [0,0]
+		@datos_desktop_fijo_diario = [0,0]
+		@datos_desktop_pyme_fijo_diario = [0,0]
+		@datos_desktop_pyme_movil_diario = [0,0]
 	end
   end
 end
